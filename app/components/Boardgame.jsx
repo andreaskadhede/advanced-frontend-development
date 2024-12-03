59c08b0fc78ff6726bec9eb31631636d395657c6
simon767b <simon767b@gmail.com>
2024-12-03 10:46:43 +0100

    billeder virker

--- app/components/Boardgame.jsx
+++ app/components/Boardgame.jsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 // import styles from "./page.module.css";
 
-export default function Boardgame() {
+export default function Boardgame({ post }) {
   return (
     <div>
-      <p>Boardgame</p>
+      <Image
+        src={post.acf.image}
+        alt={post.title.rendered}
+        width={400}
+        height={200}
+        className="w-full h-48 object-cover"
+      />
+      <p>{post.title.rendered}</p>
     </div>
   );
 }
