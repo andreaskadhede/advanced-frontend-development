0c40424dbd01a909b874ffca08a2f5243b4439bc
andreaskadhede <andreaskadhede@icloud.com>
2024-12-03 14:18:46 +0100

    inspiration styling

--- app/inspiration/page.jsx
+++ app/inspiration/page.jsx
@@ -3,8 +3,74 @@ import Image from "next/image";
 
 export default function Inspiration() {
    return (
-      <main>
-         <p>Inspiration</p>
+      <main className="inspiration_main">
+         <Image
+            className="inspiration_background"
+            src="/img/inspiration_background.svg"
+            height={1000}
+            width={1000}
+            alt="Inspirations lokale"
+         />
+
+         {/* <Image
+            className="bowl"
+            src="/gif/bowl.gif"
+            height={400}
+            width={400}
+            alt="Lodtrækningsskål"
+         />
+         <Image
+            className="bowl"
+            src="/img/bowl.svg"
+            height={400}
+            width={400}
+            alt="Lodtrækningsskål"
+         />
+
+         <Image
+            className="pinboard"
+            src="/gif/pinboard.gif"
+            height={400}
+            width={400}
+            alt="Opslagstavle"
+         />
+         <Image
+            className="pinboard"
+            src="/img/pinboard.svg"
+            height={400}
+            width={400}
+            alt="Opslagstavle"
+         />
+
+         <Image
+            className="posters"
+            src="/gif/posters.gif"
+            height={400}
+            width={400}
+            alt="Plakater"
+         />
+         <Image
+            className="posters"
+            src="/img/posters.svg"
+            height={400}
+            width={400}
+            alt="Plakater"
+         />
+
+         <Image
+            className="wheel_high"
+            src="/gif/wheel_legs_high.gif"
+            height={400}
+            width={400}
+            alt="Lykkehjul"
+         />
+         <Image
+            className="wheel_high"
+            src="/img/wheel_legs_high.svg"
+            height={400}
+            width={400}
+            alt="Lykkehjul"
+         /> */}
       </main>
    );
 }
