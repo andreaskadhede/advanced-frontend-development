f5b47e167e95b456dabb74284878c3f792f17083
Inger-Margrethe <imhp@hotmail.dk>
2024-12-03 11:43:53 +0100

    home edits

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,36 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <section><h2></h2></section>
+      <section className="shelf">
+        <h2>Populære spil</h2>
+      </section>
       <div>
-        <section></section>
-        <section></section>
+        <section className="shelf">
+          <Image src="/img/bell.svg" />
+          <Image
+            src="/img/plant1_small.svg"
+            height={400}
+            width={400}
+            alt="Potteplante"
+          />
+        </section>
+        <section className="shelf">
+          <h3>Bar</h3>
+        </section>
       </div>
       <div>
-        <section></section>
-        <section></section>
+        <section className="shelf">
+          <h3>Skjulte perler</h3>
+        </section>
+        <section className="shelf">
+          <Image
+            src="/gif/wheel_legs_low.gif"
+            height={400}
+            width={400}
+            alt="Lykkehjul"
+          />
+          <h3>Inspiration</h3>
+        </section>
       </div>
     </main>
   );
