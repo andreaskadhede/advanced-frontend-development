fe33c1efae6ea1f20d27fc4e2b35d2cb60775469
simon767b <simon767b@gmail.com>
2024-12-03 10:49:28 +0100

    nu kan man gå til individuelle sider

--- app/components/Boardgame.jsx
+++ app/components/Boardgame.jsx
@@ -1,17 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 // import styles from "./page.module.css";
 
 export default function Boardgame({ post }) {
   return (
-    <div>
-      <Image
-        src={post.acf.image}
-        alt={post.title.rendered}
-        width={400}
-        height={200}
-        className="w-full h-48 object-cover"
-      />
-      <p>{post.title.rendered}</p>
-    </div>
+    <Link href={`/boardgames/${post.id}`}>
+      <article
+        key={post.id}
+        className="bg-white shadow-md rounded overflow-hidden"
+      >
+        <Image
+          src={post.acf.image}
+          alt={post.title.rendered}
+          width={400}
+          height={200}
+          className="w-full h-48 object-cover"
+        />
+        <div className="p-4">
+          <h2 className="text-lg text-gray-800 font-semibold">
+            {post.title.rendered}
+          </h2>
+        </div>
+      </article>
+    </Link>
   );
 }
