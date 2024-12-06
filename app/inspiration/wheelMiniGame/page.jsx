3cc49d40347bd4092729326fe03f5927929b3e43
andreaskadhede <andreaskadhede@icloud.com>
2024-12-06 18:13:26 +0100

    wheel of fortune minigame

--- app/inspiration/wheelMiniGame/page.jsx
+++ app/inspiration/wheelMiniGame/page.jsx
@@ -1,24 +1,42 @@
+"use client";
+import Wheel from "@/app/components/Wheel";
 import Image from "next/image";
 // import styles from "./page.module.css";
+
+function spinWheel() {
+   alert("hi");
+}
 
 export default function WheelMiniGame() {
    return (
       <main className="inspiration_main">
          <div className="minigame">
-            <section className="inspo_top">
+            {/* <section className="inspo_top">
                <h1>Lykkehjulet</h1>
                <p>Spin hjulet og få et tilfældigt spilforslag!</p>
-            </section>
+            </section> */}
             <div className="inspo_bottom">
-               <Image
-                  className="wheel_low"
-                  src="/gif/wheel_legs_low.gif"
-                  height={400}
-                  width={400}
-                  alt="Lykkehjul"
-               />
+               {/* <Image
+            onClick={spinWheel}
+            className="wheel_low"
+            src="/gif/wheel_legs_low.gif"
+            height={400}
+            width={400}
+            alt="Lykkehjul"
+         /> */}
+               <div className="wheel_animation">
+                  <Image
+                     onClick={spinWheel}
+                     className="wheel_low"
+                     src="/img/wheel_legs_low.svg"
+                     height={400}
+                     width={400}
+                     alt="Lykkehjul"
+                  />
 
-               <p id="pop_1" className="box_header">
+                  <Wheel />
+               </div>
+               <p onClick={spinWheel} id="pop_1" className="box_header">
                   Start spil
                </p>
                <p id="pop_1" className="box_header">
